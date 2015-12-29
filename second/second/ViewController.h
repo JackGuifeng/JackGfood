1d8d7ace472f4d9ab1138a9b26228c03fdd8cee1
JackG <1561726629@qq.com>
2015-12-29 15:48:08 +0800

    null

---
+++ second/second/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  second
+//
+//  Created by admin on 15/12/29.
+//  Copyright © 2015年 Educational Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ second/second/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  second
+//
+//  Created by admin on 15/12/29.
+//  Copyright © 2015年 Educational Technology. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
